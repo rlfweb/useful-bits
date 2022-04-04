68a4f3f8cffc8f3df36a100c9131505cac97ff84
REBECCA FRENCH <rlfweb8@gmail.com>
2022-04-04 14:21:33 +0100

    Header

--- index.php
+++ index.php
@@ -20,13 +20,8 @@ get_header();
 		<?php
 		if ( have_posts() ) :
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
+		
+
 
 			/* Start the Loop */
 			while ( have_posts() ) :
@@ -40,12 +35,6 @@ get_header();
 				get_template_part( 'template-parts/content', get_post_type() );
 
 			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
 
 		endif;
 		?>
--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,9 +10,9 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+	<h1 class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+</h1><!-- .entry-header -->
 
 	<?php useful_bits_post_thumbnail(); ?>
 
